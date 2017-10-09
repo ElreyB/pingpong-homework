dfca6fda8dea9f1fd7d034bc5a56cfda74ad1a55
Elrey Belmonti <elbelmonti@gmail.com>
2017-10-09 14:44:16 -0700

    WIP concat

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
+var lib = require('bower-files')();
 
 gulp.task('myTask', function(){
   console.log('hello gulp');
@@ -10,5 +11,12 @@ gulp.task('jsBrowserify', function() {
   return browserify({ entries: ['./js/pingpong-interface.js'] })
     .bundle()
     .pipe(source('app.js'))
+    .pipe(gulp.dest('./build/js'));
+});
+
+gulp.task('bowerJS', function () {
+  return gulp.src(lib.ext('js').files)
+    .pipe(concat('vendor.min.js'))
+    .pipe(uglify())
     .pipe(gulp.dest('./build/js'));
 });
